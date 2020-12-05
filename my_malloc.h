1fe1128b7b1cc683786143aff386fda6ee3aefbe
samr87 <70722584+samr87@users.noreply.github.com>
2020-12-04 22:48:09 -0500

    Update my_malloc.h

--- my_malloc.h
+++ my_malloc.h
@@ -2,7 +2,7 @@
 #define MY_MALLOC_H
 using namespace std;
 
-void my_malloc(int processesMemory[], long long blockSize[], processNum) {};
+void my_malloc(int processesMemory[], long long blockSize[], int processNum) {};
 
 
 #endif
