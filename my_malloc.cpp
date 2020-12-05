bcc9a6ce20d7363d82489c31e4c04b9502ead6fe
samr87 <70722584+samr87@users.noreply.github.com>
2020-12-04 23:23:39 -0500

    Update my_malloc.cpp

--- my_malloc.cpp
+++ my_malloc.cpp
@@ -4,16 +4,15 @@
 
 using namespace std;
 
-void my_malloc(int processesMemory[], long long blockSize[], int processNum) {
+void mall::my_malloc() : my_malloc(int processesMemory[], long long blockSize[], int processNum) {
+
   int orderOfProcesses[processNum];
   
   for(int i = 0; i < 40; i++) {
     for(int j = 0; j < 40; j++) {
-      //if the block size is greater than the process's memory then it fits
       if(blockSize[j] >= processesMemory[i]) {
-      //to keep track of which processes are fit where  
         orderOfProcesses[i] = j;
-      //subtract from the total blockSize so you know how much is left to fit the other processes  
+        
         blockSize[j] - processesMemory[i];
       }
     }
