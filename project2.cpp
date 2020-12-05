bfcc79152b748f09a7123f08d72c0ca2717f00b4
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-12-04 22:52:58 -0500

    Update project2.cpp

--- project2.cpp
+++ project2.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 
 int main() {
+	
+//gets seed value from the user
     long long seedValue;
     cout << "Enter a seed value: ";
     cin >> seedValue;
@@ -23,36 +25,40 @@ int main() {
     uniform_int_distribution<long long> serv(1000000, 1000000000000);
     uniform_int_distribution<int> memReq(0, 249);
     
+//initializes all the processes to have at least 1 MB
     for(int i = 0; i < 40; i++) {
         processesMemory[i] = 1;
     }
     
+//while the total mem is not 10000, grab a random # and add it to that specific process in processesMemory
     while(totalMemUsed != 10000) {
         int mem = memReq(seed);
         processesMemory[processNum] = processesMemory[processNum] + mem;
         totalMemUsed = totalMemUsed + mem;
-        //if the totall memory over 10000 end the loop and subtract from the last process given memory        
+//if the total memory over 10000 breaks the loop       
         if(totalMemUsed > 10000) {
             break;
         }
         processNum++;
     }
-    
+	
+//subtract from the last process given memory until 10000 
     while(totalMemUsed > 10000) {
         processesMemory[processNum] = processesMemory[processNum] - 1;
         totalMemUsed = totalMemUsed - 1;
     }
-    
+//Output the processes generated    
     for(int i = 0; i < 40; i++) {
         long long servTime = serv(seed);
         int process = processesMemory[i];
         cout << "Process Number\t" << i+1 << "\tProcess Size\t" << process << "\tService Time\t" << servTime << "\n";
     }
     cout <<  "\n" << totalMemUsed << "\n";
-    
+
+//so complete simulation runs    
     bool running = true;
     
-    //while boolean is true run all of the scenarios, otherwise program will end
+//while boolean is true run all of the scenarios, otherwise program will end
     while(running != false) {
         bool scenario1 = true;
         //run scenario until false
@@ -66,9 +72,10 @@ int main() {
             processNum == 0;
             
             for(int i = 0; i < 40; i++) {
-                //in the block of 10000 mem locations malloc handles where the processes go
-
+                
+		//in the block of mem locations malloc handles where the processes go
 		p[i] = (int*)malloc(sizeof(int));
+		//take the random seed # generated and store it in the processes memory
                 int mem = memReq(seed);
                 processesMemory[i] = processesMemory[i] + mem;
                 totalMemUsed = totalMemUsed + mem;
@@ -76,12 +83,12 @@ int main() {
             }
             processNum++;
             
-
+	//freeing processes
             for(int i = 0; i < 40; i++) {
                 free(p[i]);
             }
             auto end = chrono::steady_clock::now();
-            
+       //outputting data     
             for(int i = 0; i < 40; i++) {
                 long long servTime = serv(seed);
                 int process = processesMemory[i];
@@ -93,7 +100,7 @@ int main() {
         }
 	    
 	bool scenario2 = true;
-	long long blockSize{10000];    
+	long long blockSize[10000];    
 	while(scenario2 != false) {
 		auto start = chrono::steady_clock::now();
 		
@@ -102,7 +109,8 @@ int main() {
 		}
 		
 		processNum = 40;
-		
+	
+		//call my_malloc to allocate first fit with the random seed # generated
 		for(int i = 0; i < 40; i++) {
 			int mem = memReq(seed);
 			processesMemory[i] = processesMemory[i] + mem;
