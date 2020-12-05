03fbc7c116483186c0b37f490a0327948119dac4
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-12-04 22:07:36 -0500

    Create my_malloc.cpp

---
+++ my_malloc.cpp
@@ -0,0 +1,24 @@
+#include "my_malloc.h"
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+void my_malloc(int processesMemory[], int blockSize[], int processNum) {
+  int orderOfProcesses[processNum];
+  
+  for(int i = 0; i < 40; i++) {
+    for(int j = 0; j < 40; j++) {
+      if(blockSize[j] >= processesMemory[i]) {
+        orderOfProcesses[i] = j;
+        
+        blockSize[j] - processesMemory[i];
+      }
+    }
+  }
+  cout << "Allocation Complete\n" << "The order the processes were processed are: \n";
+  for(int i = 0; i < 40; i++) {
+    int order = orderOfProcesses[i];
+    cout << order;
+  }
+}
